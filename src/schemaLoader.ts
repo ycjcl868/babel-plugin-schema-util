07278d21c4ea6a329e24bb42fa67984d11ffad08
ycjcl868 <45808948@qq.com>
2019-05-21 18:41:27 +0800

    :lipstick: .schema Loader json stringify

--- src/schemaLoader.ts
+++ src/schemaLoader.ts
@@ -1,6 +1,8 @@
 import { schema } from 'schema-util';
 
 export default (content: string) => {
-  const res = JSON.stringify(schema(content));
+  const res = JSON.stringify(schema(content))
+    .replace(/\u2028/g, '\\u2028')
+    .replace(/\u2029/g, '\\u2029');
   return res;
 };
